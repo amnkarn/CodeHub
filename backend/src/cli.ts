3d5acc1f890c3b7c49c7d45f7157306df9863bf3
Aman Karn <amankarn2007@gmail.com>
2026-04-23 09:22:46 +0530

    codeHub commads

---
+++ backend/src/cli.ts
@@ -0,0 +1,52 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers"
+import { commitRepo, init, pullRepo, pushRepo, revertRepo } from "./controller/command.controller.js";
+import { addRepo } from "./controller/command.controller.js";
+
+
+// command controller
+yargs(hideBin(process.argv))
+    .command('init', 'Initialise a new repository', {}, init)
+
+    .command('push', 'Push commits to CodeHub', {}, pushRepo)
+
+    .command('pull', 'Pull commits from CodeHub', {}, pullRepo)
+
+    .command(
+        'add <file>',
+        'Add a file to repository',
+        (yargs) => {
+            return yargs.positional("file", {
+                describe: "File to add to the staging area",
+                type: "string"
+            })
+        },
+        addRepo
+    )
+
+    .command(
+        "commit",
+        "Commit the staged file",
+        (yargs) => {
+            return yargs.positional("message", {
+                describe: "Commit message",
+                type: "string"
+            })
+        },
+        commitRepo
+    )
+
+    .command(
+        "revert <commitID>",
+        "Revert to specific commit",
+        (yargs) => {
+            return yargs.positional("commitId", {
+                describe: "Commit ID to revert to",
+                type: "string"
+            })
+        },
+        revertRepo
+    )
+
+    .demandCommand(1, "you need atleast one command")
+    .help().argv;
--- backend/src/controller/command.controller.ts
+++ backend/src/controller/command.controller.ts
@@ -1,9 +1,79 @@
+import fs from "fs/promises";
+import path from "path";
+
+//import { exec } from "child_process";
+//import { promisify } from "util";
+//const execPromise = promisify(exec);
 
 
-export function init() {
-    console.log("init command called");
+export async function init() {
+    const repoPath = path.resolve(process.cwd(), ".codeHub"); //hidden folder
+    const commitPaths = path.resolve(repoPath, "commits"); //subfolder
+
+    try {
+        //create folders
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitPaths, { recursive: true });
+        
+        // write config file
+        const config = { bucket: process.env.S3_BUCKET || "default-bucket" };
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringify(config, null, 2),
+        )
+
+        //if (process.platform === "win32") { //hide .codeHub folder
+        //    await execPromise(`attrib +h "${repoPath}"`);
+        //}
+
+        console.log("Repository initialized successfully at .codeHub");
+
+    } catch (error) {
+        console.log("Error in intialising the repository", error);
+    }
 }
 
-export function addRepo() {
+export async function addRepo(argv: any) {
+    const repoPath = path.resolve(process.cwd(), ".codeHub"); //.codeHub path
+    const stagingPath = path.resolve(repoPath, "staging"); //staging dir
+    try {
+        //string from 'add <file>'
+        const fileToStaging = argv.file; //hello.txt
+        if(!fileToStaging) {
+            throw new Error("No file path provided.");
+        }
+
+        const filePath = path.resolve(process.cwd(), fileToStaging); //absolute path of src file
+
+        const fileName = path.basename(filePath); //name of the file
+
+        await fs.mkdir(stagingPath, { recursive: true }); //create staging dir
+        const destPath = path.join(stagingPath, fileName); //destination path
+
+        await fs.copyFile(filePath, destPath);
+        console.log(`File ${fileName} added to the staging area`);
+
+    } catch (error: any) {
+        if (error.code === 'ENOENT') {
+            console.error(`Error: The file "${argv.file}" does not exist.`);
+        } else {
+            console.error("Error adding file: ", error.message);
+        }
+    }
+}
+
+export function commitRepo() {
+    console.log("add repo command called")
+}
+
+export function pushRepo() {
+    console.log("add repo command called")
+}
+
+export function pullRepo() {
+    console.log("add repo command called")
+}
+
+export function revertRepo() {
     console.log("add repo command called")
 }
